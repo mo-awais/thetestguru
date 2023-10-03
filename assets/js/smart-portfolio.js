8315493612da6cd6366da35fa9d39ba3d51b3c2c
Mohammed Awais <awais@mohammedawais.me>
2023-10-03 19:12:58 +0100

    Delete old website files

--- assets/js/smart-portfolio.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function (){
-    $("#services-nav-link").click(function (){
-        $('html, body').animate({
-            scrollTop: $("#services").offset().top
-        }, 2000);
-    });
-});
